e3f91a16dc3230e8430acd03ec801e51d46c31c2
Kyle Groening <kyle.groening@cytiva.com>
2022-11-16 20:03:52 -0500

    Add additional pseudocode to expand the program

--- game.js
+++ game.js
@@ -3,6 +3,7 @@
 let userChoice = prompt('Rock, Paper, or Scissors? ')
 alert(userChoice)
 alert(getComputerChoice())
+// Function to Validate user input to ensure it's a legal choice
 // Create string variable to hold game choice from computer function
 // Write function to randomly select computer game choice and save in variable
 
@@ -13,3 +14,7 @@ function getComputerChoice(){
 }
 // Compare user choice to computer choice to determine winner
 // Print user choice and computer choice on screen, along with the winner
+// Set up for best of 5 rounds - whoever wins 3 rounds first wins the game
+// Loop through function to run rounds until there is a winner
+// Create integer variable to keep track of computer vs user score
+// Prompt to play again and reset the tracker variables
