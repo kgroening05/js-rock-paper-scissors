8786ffb8132ed9197f8637c4f885a11ac6bee6e4
Kyle Groening <kyle.groening@cytiva.com>
2022-11-16 19:58:28 -0500

    Add function to get random int between 0 and 2

--- game.js
+++ game.js
@@ -2,7 +2,14 @@
 // Store input from browser prompt from user in variable
 let userChoice = prompt('Rock, Paper, or Scissors? ')
 alert(userChoice)
+alert(getComputerChoice())
 // Create string variable to hold game choice from computer function
 // Write function to randomly select computer game choice and save in variable
+
+function getComputerChoice(){
+    let numberCap = 3
+    let randomChoice = Math.floor(Math.random() * numberCap);
+    return randomChoice
+}
 // Compare user choice to computer choice to determine winner
 // Print user choice and computer choice on screen, along with the winner
