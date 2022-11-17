e3fe6f3064b929ee02cb498642b7768d6cde69a8
Kyle Groening <kyle.groening@cytiva.com>
2022-11-16 19:46:26 -0500

    Add initial boilerplate html, script with pseudcode

---
+++ game.js
@@ -0,0 +1,8 @@
+// Create string variable to hold game choice from user
+// Store input from browser prompt from user in variable
+let userChoice = prompt('Rock, Paper, or Scissors? ')
+alert(userChoice)
+// Create string variable to hold game choice from computer function
+// Write function to randomly select computer game choice and save in variable
+// Compare user choice to computer choice to determine winner
+// Print user choice and computer choice on screen, along with the winner
